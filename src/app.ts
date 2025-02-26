fb771b8609a92a326abaaceb0b2a0c45e2972d62
Irvan Malik <irvanma@gnuweeb.org>
2025-02-26 19:42:23 +0700

    feat(app): modify
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import { Elysia } from "elysia";
 import { node } from "@elysiajs/node";
+import { AppRoutes } from "./app.routes";
 
-const app = new Elysia({ adapter: node() })
-  .get("/", () => "Hello Elysia")
-  .listen(3000, ({ hostname, port }) => {
-    console.log(`🦊 Elysia is running at ${hostname}:${port}`);
-  });
+const app = new Elysia({ adapter: node() }).use(AppRoutes);
+
+app.listen(5128, () => {
+  console.log(
+    `[A-E] Server is running on port ${app.server?.hostname}:${app.server?.port}`
+  );
+});
