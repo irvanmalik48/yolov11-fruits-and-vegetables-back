2c37ac14ceeb8daad120a79b4fe1081bb1ae9357
Irvan Malik <irvanma@gnuweeb.org>
2025-02-26 19:58:32 +0700

    feat(app): add ooga booga route guard because people might not care about the prefix
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,31 @@
 import { Elysia } from "elysia";
 import { node } from "@elysiajs/node";
+import fastJson from "fast-json-stringify";
 import { AppRoutes } from "./app.routes";
 
-const app = new Elysia({ adapter: node() }).use(AppRoutes);
+const app = new Elysia({ adapter: node() }).use(AppRoutes).get("/", () => {
+  const wrongRootSchema = fastJson({
+    title: "You are in the wrong place",
+    type: "object",
+    properties: {
+      prefix: { type: "string" },
+      message: { type: "string" },
+      visit: { type: "string" },
+    },
+  });
+
+  const message = wrongRootSchema({
+    prefix: "v1",
+    message: "You are in the wrong root route. Please visit `/v1`.",
+    visit: "/v1",
+  });
+
+  if (process.env.NODE_ENV === "development")
+    console.log("[A-E] `/` accessed. You shouldn't be here.");
+
+  return message;
+});
 
 app.listen(5128, () => {
-  console.log(
-    `[A-E] Server is running on port ${app.server?.hostname}:${app.server?.port}`
-  );
+  console.log(`[A-E] Server is running on port 5128.`);
 });
